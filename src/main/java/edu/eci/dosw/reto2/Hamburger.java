2c4b1a8a6b1630e233074f4898076ae066e5a804
Juan José Rivera López <juanjorivlo@gmail.com>
2026-08-18 11:38:41 -0500

    Implemento of setIngredints

--- src/main/java/edu/eci/dosw/reto2/Hamburger.java
+++ src/main/java/edu/eci/dosw/reto2/Hamburger.java
@@ -32,6 +32,6 @@ public class Hamburger {
      * Method needed to set the ingredients of the burger after being built
      */
     public void setIngredients(ArrayList ingred){
-        //Implementation
+        this.ingredients = ingred;
     }
 }
