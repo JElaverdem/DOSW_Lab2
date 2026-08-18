6626ae77e3aad320075e44505473561bf86ca3fd
Juan José Rivera López <juanjorivlo@gmail.com>
2026-08-18 11:17:07 -0500

    Actualization of challenge 2, calss burger and ingredient

---
+++ src/main/java/edu/eci/dosw/reto2/Ingredient.java
@@ -0,0 +1,14 @@
+public class Ingredient {
+    public String name;
+    public double price;
+    public Ingredient(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+    public String getname() {
+        return name;
+    }
+    public double getprice() {
+        return price;
+    }
+}
