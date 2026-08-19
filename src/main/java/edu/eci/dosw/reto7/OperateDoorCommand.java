181e3e45ab9ce1a2f6fe76e95dc7ba3c647d6435
Juan Jose Rivera Lopez <juan.rivera@escuelaing.edu.co>
2026-08-19 18:22:11 -0500

    add the MoveBlindCommand, RemoteControl, User, door, OperateDoorCommand

---
+++ src/main/java/edu/eci/dosw/reto7/OperateDoorCommand.java
@@ -0,0 +1,24 @@
+package edu.eci.dosw.reto7;
+
+public class OperateDoorCommand implements Command {
+    private Door door;
+    private boolean newState;
+    private boolean previousState;
+
+    public OperateDoorCommand(Door door, boolean open) {
+        this.door = door;
+        this.newState = open;
+    }
+
+    @Override
+    public void execute() {
+        this.previousState = door.isOpen();
+        this.door.setOpen(newState);
+    }
+
+    @Override
+    public void undo() {
+        System.out.println("return the last state...");
+        this.door.setOpen(previousState);
+    }
+}
--- src/main/java/edu/eci/dosw/reto7/RemoteControl.java
+++ src/main/java/edu/eci/dosw/reto7/RemoteControl.java
@@ -1,5 +1,45 @@
 package eci.dosw.reto7;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RemoteControl {
-    
+    private List<ActionRecord> history;
+
+    public RemoteControl() {
+        this.history = new ArrayList<>();
+    }
+
+    public void executeCommand(Command command, User user) { 
+        command.execute();
+        ActionRecord record = new ActionRecord(user, command);
+        history.add(record);
+    }
+
+    public void undoLast() {
+        if (history.isEmpty()) {
+            System.out.println("There aren't actions in the record");
+            return;
+        }
+        
+        for (int i = history.size() - 1; i >= 0; i--) {
+            ActionRecord record = history.get(i);
+            if (!record.isUndone()) {
+                // AQUÍ ESTÁ LA LÍNEA QUE FALTABA
+                record.getCommand().undo(); 
+                
+                record.markAsUndone();
+                return;
+            }
+        }
+        System.out.println("All actions are disabled.");
+    }
+
+    public void printSummary() {
+        System.out.println("--- Remote Control record ---");
+        for (ActionRecord record : history) {
+            String estado = record.isUndone() ? "[UNDONE]" : "[ACTIVE]";
+            System.out.println(estado + " Action by user.");
+        }
+    }
 }
