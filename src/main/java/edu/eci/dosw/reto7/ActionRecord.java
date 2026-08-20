7bd3e1922dc8b0ccaa327710d31ee9c180a6da8a
JElaverdem <juan.lmesa@mail.escuelaing.edu.co>
2026-08-19 20:00:50 -0500

    Adding Command, ActionRecord, Light and AdjustVolumeCommand

--- src/main/java/edu/eci/dosw/reto7/ActionRecord.java
+++ src/main/java/edu/eci/dosw/reto7/ActionRecord.java
@@ -1,5 +1,22 @@
 package eci.dosw.reto7;
 
+import java.time.LocalDateTime;
+
 public class ActionRecord {
-    
+    private User user;
+    private Command command;
+    private boolean undone;
+    private LocalDateTime timestamp;
+    public ActionRecord(User user, Command command){
+        this.user=user;
+        this.command=command;
+        this.undone=true;
+        this.timestamp=LocalDateTime.now();
+    }
+    public boolean isUndone(){
+        return this.undone;
+    }
+    public void markAsUndone(){
+        this.undone=false;
+    }
 }
--- src/main/java/edu/eci/dosw/reto7/Command.java
+++ src/main/java/edu/eci/dosw/reto7/Command.java
@@ -1,5 +1,6 @@
 package eci.dosw.reto7;
 
 public interface Command {
-    
+    public abstract void execute();
+    public abstract void undo();
 }
